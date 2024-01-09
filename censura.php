e341856d4eeec4b0f521f88e2a5a57a5e4ed3f0c
AlessandroBilli <skillmanbungo@gmail.com>
2024-01-09 10:30:26 +0100

    fix. strcuture php

---
+++ censura.php
@@ -0,0 +1,18 @@
+
+<?php 
+$parola_censurata = $_GET ['paragrafo'];
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <h1>Parola censurata </h1>
+    <p> <?php echo $paragrafo?></p>
+</body>
+</html>
